a19aa4c84820e5e08747ec57fd9476e2364b85dd
Eric Cheung <eric1162004@hotmail.com>
2022-02-07 17:59:20 -0800

    Initial Commit

---
+++ AnimalClassifierApp/AnimalClassifierAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalClassifierAppApp.swift
+//  AnimalClassifierApp
+//
+//  Created by Eric Cheung on 2022-02-07.
+//
+
+import SwiftUI
+
+@main
+struct AnimalClassifierAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
