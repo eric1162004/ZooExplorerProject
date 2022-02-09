2cf918f5fb081bd45704f5d1112daa6b2992d27f
Eric Cheung <eric1162004@hotmail.com>
2022-02-08 18:29:52 -0800

    using custom classifier model

--- AnimalClassifierApp/Services/ImageClassifier.swift
+++ AnimalClassifierApp/Services/ImageClassifier.swift
@@ -19,7 +19,9 @@ class ImageClassifier {
         }
         
         // try to initialize animal classier model
-        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { fatalError("Loading CoreML Model Failed")}
+        guard let model = try? VNCoreMLModel(for: AnimalClassifier().model) else { fatalError("Loading CoreML Model Failed")}
+        
+//        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { fatalError("Loading CoreML Model Failed")}
         
         // construct a prediction request
         let request = VNCoreMLRequest(model: model) { request, Error in
@@ -29,12 +31,20 @@ class ImageClassifier {
             }
         
             // get the first word in the results
+//            if let firstResult = results.first {
+//
+//                let animalName = firstResult.identifier.components(separatedBy: ",")[0]
+////                let animalName = firstResult.identifier
+//                completionHandler(.success(animalName))
+//            }
+            
             if let firstResult = results.first {
                 
-                let animalName = firstResult.identifier.components(separatedBy: ",")[0]
-//                let animalName = firstResult.identifier
+                let animalName = firstResult.identifier
+                print("predicted anmial:", animalName)
                 completionHandler(.success(animalName))
             }
+            
         }
         
         let handler = VNImageRequestHandler(ciImage: ciImage)
